ea68dbc06767b4040e5003a6ef930d643b037932
Balogun2705 <balogunsemiat2019@gmail.com>
2024-12-16 23:48:49 +0100

    just go abeg

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar';
-import { UserProvider } from './context/UserContext.jsx';
+// import { UserProvider } from './context/UserContext.jsx';
 
 const App = () => {
   return (
     <>
-      <UserProvider>
+      {/* <UserProvider> */}
         <Navbar />
         <main className='min-h-screen font-primary'>
         <Outlet /> 
         </main>
         <h2>Footer</h2>
-      </UserProvider>
+      {/* </UserProvider> */}
     </>
   )
 }
