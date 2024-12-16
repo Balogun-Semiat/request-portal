55aa569f22e6b95e0fe01b0545e5ab1fafc12866
Balogun2705 <balogunsemiat2019@gmail.com>
2024-12-16 23:41:36 +0100

    removed unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar';
-import { UserProvider } from './context/userContext';
+import { UserProvider } from './context/UserContext';
 
 const App = () => {
   return (
